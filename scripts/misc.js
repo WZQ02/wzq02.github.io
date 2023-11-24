d553303d53fe9ab70f01e7c781f29855e2ebeef5
WZQ02 <ziqiwang2002@Hotmail.com>
2023-11-24 17:09:36 +0800

    friend translation blahblafhewfgieagwir

--- scripts/misc.js
+++ scripts/misc.js
@@ -1,7 +1,7 @@
 var a_sc_group;
 
 window.onload = function() {
-    document.getElementsByClassName("stitle2")[20].style.display = "none";
+    document.getElementsByClassName("stitle2")[19].style.display = "none";
     a_sc_group = document.getElementsByClassName("article_shortcut");
     mdcontent_setatt(a_sc_group,"none")
 }
@@ -11,7 +11,7 @@ var showstitle13_counter = 0
 function showstitle13() {
     showstitle13_counter++;
     if (showstitle13_counter > 2) {
-        document.getElementsByClassName("stitle2")[20].style.display = ""
+        document.getElementsByClassName("stitle2")[19].style.display = ""
     }
 }
 
